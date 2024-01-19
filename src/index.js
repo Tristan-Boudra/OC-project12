7f62720db8db433eff692b6214236a3586a2e0aa
Tristan-Boudra <boudratristan@gmail.com>
2024-01-19 09:17:23 +0100

    fix(routes): change on new version of routes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/main.scss";
 import Error from "./components/Error";
 import App from "./pages/Home";
 import Login from "./pages/Login";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/user/:userId" element={<App />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/user/:userId",
+    element: <App />,
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
